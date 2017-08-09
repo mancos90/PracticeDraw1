a72f9cafd24cd19f11afa629ee133d7b006b2add
mancos <mancos90@gmail.com>
2017-08-09 10:51:59 +0800

    commit some

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +27,15 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.translate(canvas.getWidth() / 2, canvas.getHeight() / 2);
+        RectF rectF = new RectF(-200, -120, 200, 120);
+        canvas.drawArc(rectF, 180f, 60f, false, paint);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(rectF, 250f, 100f, true, paint);
+
+        canvas.drawArc(rectF, 10f, 160f, false, paint);
     }
 }
