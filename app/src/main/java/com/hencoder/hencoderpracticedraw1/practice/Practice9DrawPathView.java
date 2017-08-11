4e574fd923b9d58c8fe77c31eb488447e843ce1e
mancos <mancos90@gmail.com>
2017-08-11 14:54:06 +0800

    commit Practice 9

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,7 +2,11 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,10 +24,20 @@ public class Practice9DrawPathView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+
+        Paint paint = new Paint();
+        Path path = new Path();
+        canvas.translate(canvas.getWidth()/2,canvas.getHeight()/2);
+        path.addArc(-200, -200, 0, 0, -225, 225);
+        path.arcTo(0, -200, 200, 0, -180, 225, false);
+        path.lineTo(0, 150);
+        canvas.drawPath(path, paint);
     }
 }
