7ae50b600466d27ca560378d8aa199d0e0e7540f
mancos <mancos90@gmail.com>
2017-08-11 14:53:29 +0800

    commit Practice 10

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -1,7 +1,9 @@
 package com.hencoder.hencoderpracticedraw1.practice;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -20,11 +22,14 @@ public class Practice10HistogramView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+
     }
 }
